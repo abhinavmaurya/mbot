e15cdba75073efbbedf48fe5c2e364caedce9de8
Sabrish Ramamoorthy <sramamoorthy@mobileaware.com>
2016-11-03 19:13:58 -0400

    a

--- server.js
+++ server.js
@@ -1,16 +1,97 @@
-var express = require('express');
-var app = express();
+/**
+ * Created by abhinavmaurya on 11/3/16.
+ */
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express()
+app.set('port', (process.env.PORT || 5000))
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+// Process application/json
+app.use(bodyParser.json())
+// Index route
+app.get('/', function (req, res) {
+    res.send('Hello world, I am a chat bot')
+})
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'mBot') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+           // sendTextMessage(sender, "Text received, echo: " + JSON.stringify(req.body))
+            checkLogin(sender, "Text received, echo: " + JSON.stringify(req.body))
+        }
+    }
+    res.sendStatus(200)
+})
+var token = "EAAXNSn977yoBAAz62l0C8mTv01p4wkNAqjZCPBwltCbjZBZB2QRZA7CRDsI13pZCEK8hKkJaw1gXZAb9iV2qw8aKbTMbcyY2aZCNZC0aCV1IDzA0hlM7m40TxOk2jAQ17S1PTzBhBLiYp7oPbFxxXwhEw3c7lRqaUZAMwrZAUFMtWSZCQZDZD"
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+function checkLogin(sender, text){
 
-// configure a public directory to host static content
-app.use(express.static(__dirname + '/public'));
+    messageData = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Welcome to MBot",
+                        "image_url": "http://www.example.com/images/m-bank.png",
+                        "buttons": [{
+                            "type": "account_link",
+                            "url": "https://www.example.com/authorize"
+                        }]
+                    }]
+                }
+            }
+        }
 
-require ("./test/app.js")(app);
-
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-
-app.listen(port, ipaddress);
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
