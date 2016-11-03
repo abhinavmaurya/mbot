75855538467b68939209286f31055d46aadafc24
System Administrator <root@Abhinavs-MacBook-Pro.local>
2016-11-03 14:26:59 -0400

    frst api

--- test/app.js
+++ test/app.js
@@ -1,41 +1,32 @@
 module.exports = function(app)
 {
     app.get("/api/test", findAllMessages);
-    app.post("/api/test", createMessage);
-    app.delete("/api/test/:id", deleteMessage);
+    /*app.post("/api/test", createMessage);
+    app.delete("/api/test/:id", deleteMessage);*/
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test';
+    /*var connectionString = 'mongodb://127.0.0.1:27017/test';*/
 
-    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    /*if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
             process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
             process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
             process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
             process.env.OPENSHIFT_APP_NAME;
-    }
+    }*/
 
-    var mongoose = require("mongoose");
+    /*var mongoose = require("mongoose");
     mongoose.connect(connectionString);
 
     var TestSchema = mongoose.Schema({
         message: String
-    });
+    });*/
 
-    var TestModel = mongoose.model("TestModel", TestSchema);
+    //var TestModel = mongoose.model("TestModel", TestSchema);
 
     function findAllMessages(req, res) {
-        TestModel
-            .find()
-            .then(
-                function(tests) {
-                    res.json(tests);
-                },
-                function(err) {
-                    res.status(400).send(err);
-                }
-            );
+        res.json({"abc":"abc"});
     }
-
+    /*
     function createMessage(req, res) {
         TestModel
             .create(req.body)
@@ -60,5 +51,5 @@ module.exports = function(app)
                     res.status(400).send(err);
                 }
             );
-    }
+    }*/
 };
