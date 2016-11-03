95d4e5baefc30c1b7cb77ab3aa6a4ac373dfa5b6
System Administrator <root@Abhinavs-MacBook-Pro.local>
2016-11-03 14:48:38 -0400

    login response

--- test/app.js
+++ test/app.js
@@ -1,55 +1,29 @@
 module.exports = function(app)
 {
     app.get("/api/test", findAllMessages);
-    /*app.post("/api/test", createMessage);
-    app.delete("/api/test/:id", deleteMessage);*/
-
-    /*var connectionString = 'mongodb://127.0.0.1:27017/test';*/
-
-    /*if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-            process.env.OPENSHIFT_APP_NAME;
-    }*/
-
-    /*var mongoose = require("mongoose");
-    mongoose.connect(connectionString);
-
-    var TestSchema = mongoose.Schema({
-        message: String
-    });*/
-
-    //var TestModel = mongoose.model("TestModel", TestSchema);
 
     function findAllMessages(req, res) {
-        res.json({"abc":"abc"});
-    }
-    /*
-    function createMessage(req, res) {
-        TestModel
-            .create(req.body)
-            .then(
-                function(test) {
-                    res.json(test);
-                },
-                function(err) {
-                    res.status(400).send(err);
+        loginJson = {
+            "recipient":{
+                "phone_number":"+1(562)583-6629"
+            },
+            "message": {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [{
+                            "title": "Welcome to M-bot",
+                            "image_url": "http://www.mobileaware.com/wp-content/uploads/2013/04/twitter_avatar.png",
+                            "buttons": [{
+                                "type": "account_link",
+                                "url": "https://www.example.com/authorize"
+                            }]
+                        }]
+                    }
                 }
-            );
+            }
+        };
+        res.json(loginJson);
     }
-
-    function deleteMessage(req, res) {
-        TestModel
-            .remove({_id: req.params.id})
-            .then(
-                function(result) {
-                    res.json(result);
-                },
-                function(err) {
-                    res.status(400).send(err);
-                }
-            );
-    }*/
 };
